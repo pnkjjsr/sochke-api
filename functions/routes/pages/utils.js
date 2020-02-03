e800698c27dd3379536342cc9ddad88aa69f6e18
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2020-02-03 20:50:51 +0530

    fix: promoted id changes.

--- functions/routes/pages/utils.js
+++ functions/routes/pages/utils.js
@@ -1,9 +1,5 @@
 exports.randomUID = () => {
-  let uidArr = [
-    "qwO50tOcKuPlXgz9hni4TSxeIiw1",
-    "vSIs7pkFOrfxf0BZtTeQ4qrSJCE2",
-    "OxFxRzjfOma11xuXFIYyVYGhDKU2"
-  ];
+  let uidArr = ["POvOpBFRcKZh9eGF0BAmr0dfd0K3", "iX90XjJCawdgOwbVPNNHVNoUxpc2"];
   var randUID = uidArr[Math.floor(Math.random() * uidArr.length)];
   return randUID;
 };
